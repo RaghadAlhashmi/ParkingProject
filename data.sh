7a2cb4d49e5160015a7f89a7ef0960e9909073d9
RaghadAlhashmi <s444000928@st.uqu.edu.sa>
2024-05-18 00:42:07 +0300

    Raghad's code

---
+++ data.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#File paths
+parking_data="parking_data.txt"
+if [ ! -f "$parking_data" ]
+ then
+     touch "parking_data"
+fi
+
+
+#Initializing available spots
+initialize_spots() {
+ local i=1
+ while [ $i -le 50 ]
+ do
+   echo $i
+   i=$((i + 1))
+ done > available_spots.txt
+}
+
+#if available_spots file doesnt exist run initialization
+if [ ! -f "available_spots.txt" ]
+ then
+     initialize_spots
+fi
+
+#show available parking spots
+show_available_spots() {
+ echo " Available parking spots:"
+ cat available_spots.txt
