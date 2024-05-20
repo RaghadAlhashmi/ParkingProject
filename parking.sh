90961c20f632a8fce51b11075aa2f3e1ae84ad83
RaghadAlhashmi <s444000928@st.uqu.edu.sa>
2024-05-20 19:23:42 +0300

    fixing errors

--- parking.sh
+++ parking.sh
@@ -5,7 +5,7 @@ source data.sh
 # generating parking tickets
 generate_ticket_id() {
   local last_ticket=$(tail -1 "parking_data" | awk -F, '{print $1}' | sed 's/TICKET//')
-  last_ticket=$((last_ticket + 1))
+  local ticket_number=$((last_ticket + 1))
   echo "TICKET$last_ticket"
 }
 
